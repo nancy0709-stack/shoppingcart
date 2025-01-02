cfdbb3c07e2977f587fd4999f059919ef7444bd6
nancy0709-stack <nancydas07092000@gmail.com>
2025-01-02 14:03:23 +0530

    name

---
+++ src/com/nancy/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.nancy.service;
+
+import java.util.List;
+
+import com.nancy.beans.OrderBean;
+import com.nancy.beans.OrderDetails;
+import com.nancy.beans.TransactionBean;
+
+public interface OrderService {
+
+	public String paymentSuccess(String userName, double paidAmount);
+
+	public boolean addOrder(OrderBean order);
+
+	public boolean addTransaction(TransactionBean transaction);
+
+	public int countSoldItem(String prodId);
+
+	public List<OrderBean> getAllOrders();
+
+	public List<OrderBean> getOrdersByUserId(String emailId);
+
+	public List<OrderDetails> getAllOrderDetails(String userEmailId);
+
+	public String shipNow(String orderId, String prodId);
+}
---
+++ src/com/nancy/service/TransService.java
@@ -0,0 +1,6 @@
+package com.nancy.service;
+
+public interface TransService {
+
+	public String getUserId(String transId);
+}
